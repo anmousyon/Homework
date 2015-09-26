348c7f17f54476b98fbbf148bd2551b385e0fc64
Jared Mosley <anmousyon@gmail.com>
2015-09-26 15:45:04 -0500

    Updated ArrayQueue

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -4,14 +4,14 @@ public class ArrayQueue {
 	private int size;
 	private int front;
 	private int back;
-	
+
 	public ArrayQueue(){
 		queueArray = new String[100];
 		size = 0;
 		front = 0;
 		back = -1;
 	}
-	
+
 	public ArrayQueue(int startSize){
 		queueArray = new String[startSize];
 		size = 0;
@@ -31,34 +31,53 @@ public class ArrayQueue {
 	 */
 	public void enqueue(String toEnqueue){
 		//TODO implement enqueue
+			if(size == queueArray.length){
+				throw new IllegalStateException("Queue is full");
+			}
+			array[back] = toEnqueue;
+			back = (back+1) % queueArray.length;
+			size++;
 	}
-	
+
+
 	/**
 	 * @function removes the string from the front of the queue
 	 * @return the string from the front of the queue
 	 */
 	public String dequeue(){
 		//TODO implement dequeue
-		String toDequeue = "";
+		if(size == 0){
+			throw new NoElementException("Cannot remove from epty array");
+		}
+		String toDequeue = queueArray[front];
+		queueArray[front] = null;
+		front = (front+1)%queueArray.length;
+		sizee--;
 		return toDequeue;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return returns true if the queue is empty, false otherwise
 	 */
 	public boolean isEmpty(){
 		//TODO implement isEmpty
+		if(size!=0){
+			return false;
+		}
 		return true;
 	}
 
 	/**
-	 * 
+	 *
 	 * @return returns true if the queue is full, false otherwise
 	 */
 	public boolean isFull(){
 		//TODO implement isFull
+		if(size == queueArray.length){
+			return true;
+		}
 		return false;
 	}
-	
+
 }
